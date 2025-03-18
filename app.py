d61625962acd415344686afb0f34e91178bd32b5
mid-per <64011645@kmitl.ac.th>
2025-03-18 12:05:21 +0700

    Updated webhook code to support text, buttons, quick replies, and carousels

--- app.py
+++ app.py
@@ -1,21 +1,110 @@
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    ButtonsTemplate, TemplateSendMessage, PostbackAction,
+    QuickReply, QuickReplyButton, CarouselTemplate, CarouselColumn
+)
 
 app = Flask(__name__)
+
+# LINE Channel Access Token and Channel Secret
+line_bot_api = LineBotApi('eyJhbGciOiJIUzI1NiJ9.fdz5uIQ8ZaikjpLRfIWDdOzjShJjDIOCtpvM_cv-Gn9Znj76TZrDptiQBK8nHW-YPrvCUgYMUdb2ZJDLyUdq0t2_4QaFZw6LTOVVF7weui8j-p6AedIierI2-gE7MEDd.qV24Ify0wVrLVVGfsGDkVokRuVUZo0aWf40C8H9efaw')
+handler = WebhookHandler('a8dec9b9cff01e94ec1e3069477009fc')
 
 # Webhook route
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Get the request body as text
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # Get request body as text
     body = request.get_data(as_text=True)
-    print("Webhook received:", body)
+    app.logger.info("Request body: " + body)
 
-    # Return a success response
-    return 'Webhook received!', 200
+    # Handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-# Default route
-@app.route('/')
-def index():
-    return 'LINE Chatbot is running!'
+    return 'OK'
+
+# Handle text messages
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+
+    if user_message == "text":
+        # Reply with a text message
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="This is a text reply!")
+        )
+
+    elif user_message == "button":
+        # Reply with a button template
+        buttons_template = ButtonsTemplate(
+            title="Button Template",
+            text="Please select an option:",
+            actions=[
+                PostbackAction(label="Option 1", data="action=option1"),
+                PostbackAction(label="Option 2", data="action=option2"),
+            ]
+        )
+        template_message = TemplateSendMessage(
+            alt_text="Buttons Template",
+            template=buttons_template
+        )
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif user_message == "quickreply":
+        # Reply with quick replies
+        quick_reply_buttons = [
+            QuickReplyButton(action=PostbackAction(label="Option 1", data="action=option1")),
+            QuickReplyButton(action=PostbackAction(label="Option 2", data="action=option2")),
+        ]
+        quick_reply = QuickReply(items=quick_reply_buttons)
+        text_message = TextSendMessage(text="Choose an option:", quick_reply=quick_reply)
+        line_bot_api.reply_message(event.reply_token, text_message)
+
+    elif user_message == "carousel":
+        # Reply with a carousel template
+        carousel_template = CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url="https://example.com/image1.jpg",
+                    title="Carousel 1",
+                    text="Description 1",
+                    actions=[
+                        PostbackAction(label="Button 1", data="action=carousel1_button1"),
+                        PostbackAction(label="Button 2", data="action=carousel1_button2"),
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url="https://example.com/image2.jpg",
+                    title="Carousel 2",
+                    text="Description 2",
+                    actions=[
+                        PostbackAction(label="Button 1", data="action=carousel2_button1"),
+                        PostbackAction(label="Button 2", data="action=carousel2_button2"),
+                    ]
+                ),
+            ]
+        )
+        template_message = TemplateSendMessage(
+            alt_text="Carousel Template",
+            template=carousel_template
+        )
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    else:
+        # Default reply
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Please type 'text', 'button', 'quickreply', or 'carousel'.")
+        )
 
 if __name__ == '__main__':
     app.run(port=3000)
